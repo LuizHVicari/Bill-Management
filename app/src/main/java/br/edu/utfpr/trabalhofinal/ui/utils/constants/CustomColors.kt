80f3773e779622634f5ec5994292aae9e5506ecc
LuizHVicari <luizhenrique.vicari@gmail.com>
2024-09-30 20:40:54 -0300

    init project

---
+++ app/src/main/java/br/edu/utfpr/trabalhofinal/ui/utils/constants/CustomColors.kt
@@ -0,0 +1,10 @@
+package br.edu.utfpr.trabalhofinal.ui.utils.constants
+
+import androidx.compose.ui.graphics.Color
+
+class CustomColors {
+    companion object {
+        val INCOME_GREEN = Color(0xFF00984E)
+        val OUTCOME_RED = Color(0xFFCF5355)
+    }
+}
---
+++ app/src/main/java/br/edu/utfpr/trabalhofinal/ui/utils/constants/Sizes.kt
@@ -0,0 +1,10 @@
+package br.edu.utfpr.trabalhofinal.ui.utils.constants
+
+import androidx.compose.ui.unit.dp
+
+class Sizes {
+    companion object {
+        val SCREEN_DISTANCE_PADDING = 10.dp
+        val LIST_ITEM_SEPARATION = 50.dp
+    }
+}
